00573e0dc890c43ba0275306aa8b944e11ccc2d0
Akshay Gollapudi <123523480+Akshay201021@users.noreply.github.com>
2024-11-26 18:02:53 -0600

    Add files via upload

---
+++ hire.js
@@ -0,0 +1,6 @@
+function scrollToSection() {
+    document.getElementById('target-section').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToTop() {
+    document.querySelector('body').scrollIntoView({ behavior: 'smooth' });
+}
